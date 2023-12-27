e036f91feb5474e0e749436b7840951ed2527c17
Mert <mert.izgahi@arneca.com>
2023-12-28 01:31:49 +0300

    tests.users

--- src/__tests__/product.test.ts
+++ src/__tests__/product.test.ts
@@ -4,18 +4,29 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 const app = createServer();
 
-beforeAll(async () => {
-    const mongoServer = await MongoMemoryServer.create();
-    await mongoose.connect(mongoServer.getUri());
-});
-
 let createdProduct: any = null;
+let createdUser: any = null;
+let userPayload = {
+  email: "mert@mail.com",
+  password: "Aa123456",
+  name: "Mert",
+  phone: "123456789",
+  role: "user",
+  address: "address",
+  paymentMethod: "card",
+};
 let productPayload = {
   name: "Product 1",
   price: 100,
   description: "Product 1 description",
   category: "Laptops",
 };
+let token = "";
+beforeAll(async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
 afterAll(async () => {
   await mongoose.disconnect();
   await mongoose.connection.close();
@@ -23,6 +34,30 @@ afterAll(async () => {
 });
 
 describe("Products Controller", () => {
+  describe("POST /register", () => {
+    it("should return token with status code 201", async () => {
+      const response = await supertest(app)
+        .post("/api/v1/auth/register")
+        .send(userPayload);
+
+      token = await response.body.data;
+
+      expect(response.status).toBe(201);
+    });
+  });
+  describe("POST /login", () => {
+    it("should return token with status code 200", async () => {
+      const response = await supertest(app).post("/api/v1/auth/login").send({
+        email: userPayload.email,
+        password: userPayload.password,
+      });
+
+      token = await response.body.data;
+
+      expect(response.status).toBe(200);
+    });
+  });
+
   describe("GET /products", () => {
     it("should return products with status code 200", async () => {
       const response = await supertest(app).get("/api/v1/products");
@@ -39,4 +74,38 @@ describe("Products Controller", () => {
       expect(response.status).toBe(404);
     });
   });
+
+  describe("POST /products", () => {
+    it("should return product with status code 201", async () => {
+      const response = await supertest(app)
+        .post("/api/v1/products")
+        .set("Authorization", `Bearer ${token}`)
+        .send(productPayload);
+
+      createdProduct = await response.body.data;
+      expect(response.status).toBe(201);
+    });
+  });
+
+  describe("PUT /products/:id", () => {
+    it("should return product with status code 200", async () => {
+      const productId = createdProduct._id;
+      const response = await supertest(app)
+        .put(`/api/v1/products/${productId}`)
+        .set("Authorization", `Bearer ${token}`)
+        .send(productPayload);
+
+      expect(response.status).toBe(200);
+    });
+  });
+
+  describe("DELETE /products/:id", () => {
+    it("should return product with status code 200", async () => {
+      const productId = createdProduct._id;
+      const response = await supertest(app)
+        .delete(`/api/v1/products/${productId}`)
+        .set("Authorization", `Bearer ${token}`);
+      expect(response.status).toBe(200);
+    });
+  })
 });
---
+++ src/__tests__/users.test.ts
@@ -0,0 +1,66 @@
+import supertest from "supertest";
+import createServer from "../utils/createServer";
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+
+
+const app = createServer();
+
+let createdUser: any = null;
+let userPayload = {
+  email: "mert@mail.com",
+  password: "Aa123456",
+  name: "Mert",
+  phone: "123456789",
+  role: "user",
+  address: "address",
+  paymentMethod: "card",
+};
+
+beforeAll(async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  await mongoose.connect(mongoServer.getUri());
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongoose.connection.close();
+  createdUser = null;
+});
+
+describe("Users Controller", () => {
+  describe("POST /register", () => {
+    it("should return token with status code 201", async () => {
+      const response = await supertest(app).post("/api/v1/auth/register").send(
+        userPayload
+      );
+      expect(response.status).toBe(201);
+    });
+  });
+
+  describe("POST /login", () => {
+    it("should return token with status code 200", async () => {
+      const response = await supertest(app).post("/api/v1/auth/login").send({
+        email: userPayload.email,
+        password: userPayload.password,
+      });
+      expect(response.status).toBe(200);
+    });
+
+    it("should return error with status code 401", async () => {
+      const response = await supertest(app).post("/api/v1/auth/login").send({
+        email: userPayload.email,
+        password: "wrong-password",
+      });
+      expect(response.status).toBe(401);
+    })
+
+    it("should return error with status code 401", async () => {
+      const response = await supertest(app).post("/api/v1/auth/login").send({
+        email: "wrong-email",
+        password: userPayload.password,
+      });
+      expect(response.status).toBe(401);
+    })
+  })
+});
